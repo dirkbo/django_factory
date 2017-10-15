85db5f2c9bc949db57b03b2ff1f6288f9a844744
Dirk B <dirkbo@googlemail.com>
2017-10-15 05:07:44 +0200

    fixed url order

--- urls.py
+++ urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from django_factory import views
 
 urlpatterns = [
+     url(r'^pwa-serviceworker.js', views.pwa_serviceworker, name='pwa_serviceworker'),
      url(r'^(?P<path>.+)\.js', views.js_file, name='js_file'),
      url(r'^(?P<path>.+)\.css', views.css_file, name='css_file'),
-     url(r'^pwa-serviceworker.js', views.pwa_serviceworker, name='pwa_serviceworker'),
+
 ]
