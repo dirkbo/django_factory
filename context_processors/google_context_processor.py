761e5a87bc17fb517ebf82627ed5d0624034bffa
Dirk B <dirkbo@googlemail.com>
2019-03-08 13:15:30 +0100

    option to not google track users

--- context_processors/google_context_processor.py
+++ context_processors/google_context_processor.py
@@ -5,11 +5,19 @@ logger = logging.getLogger(__name__)
 
 
 def google_ids(request):
-    if request.user.is_staff and not getattr(settings, 'GOOGLE_TRACK_STAFF', False):
-        return {'GANALYTICS_UA': False,
-                'GADSENSE_UA': False,
-                'GOOGLE_ADS_AW': False
-                }
+    track_users = getattr(settings, 'GOOGLE_TRACK_USERS', True)
+    track_staff = getattr(settings, 'GOOGLE_TRACK_STAFF', False)
+
+    no_tracking = {'GANALYTICS_UA': False,
+                   'GADSENSE_UA': False,
+                   'GOOGLE_ADS_AW': False
+                   }
+
+    if request.user.is_staff and track_staff is False:
+        return no_tracking
+
+    if request.user.is_authenticated and track_users is False:
+        return no_tracking
 
     return {'GANALYTICS_UA': getattr(settings, 'GOOGLE_ANALYTICS_UA', False),
             'GADSENSE_UA': getattr(settings, 'GOOGLE_ADSENSE_CLIENT', False),
